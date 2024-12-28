0d4a5eaf5142075542299edf7f0eaf5a37b83af8
harrydoraemon1 <harrydoraemon1@gmail.com>
2024-12-28 21:25:45 +0530

    Add files via upload

---
+++ start_linux.sh
@@ -0,0 +1,11 @@
+export GPU_MAX_HEAP_SIZE=100
+export GPU_MAX_USE_SYNC_OBJECTS=1
+export GPU_SINGLE_ALLOC_PERCENT=100
+export GPU_MAX_ALLOC_PERCENT=100
+export GPU_MAX_SINGLE_ALLOC_PERCENT=100
+export GPU_ENABLE_LARGE_ALLOCATION=100
+export GPU_MAX_WORKGROUP_SIZE=1024
+#!/bin/sh
+reset
+
+./SRBMiner-MULTI --algorithm verushash --pool eu.luckpool.net:3956 --wallet RJ1YFPrnH8HWGE2nm46483GRsTpvwLjZBXls --password x --cpu-threads 0 --log-file ./Logs/log-linux.txt
